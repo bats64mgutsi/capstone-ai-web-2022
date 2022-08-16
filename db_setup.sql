7b707d0301b70269c42258517220145dbf78db7f
Batandwa Mgutsi <b.mgutsi@gmail.com>
2022-08-16 16:03:52 +0200

    Update db_setup.sql

---
+++ db_setup.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS authors (Surname varChar(255) NOT NULL, Initials varChar(255), Title varChar(255), Institution varChar(255), Rating Char, PRIMARY KEY(Surname));
+INSERT INTO authors (Surname, Initials, Title, Institution, Rating) VALUES ("Aharonson", "V", "Prof", "University of the Witwatersrand", "C" );
+INSERT INTO authors (Surname, Initials, Title, Institution, Rating) VALUES ("Aldrich", "C", "Prof", "Stellenbosch University", "B" );
+INSERT INTO authors (Surname, Initials, Title, Institution, Rating) VALUES ("Atemkeng Teufack", "M", "Dr", "Rhodes University", "Y" );
+INSERT INTO authors (Surname, Initials, Title, Institution, Rating) VALUES ("Bagula", "BA", "Prof", "University of the Western Cape", "C" );
+INSERT INTO authors (Surname, Initials, Title, Institution, Rating) VALUES ("Daramola", "JO", "Prof", "Cape Peninsula University of Technology", "C" );
