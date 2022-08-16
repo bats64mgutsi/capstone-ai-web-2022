8a80fc51c91e6a2821052bcd81b4cf99d4074aff
Batandwa Mgutsi <mgtbat001@sl-dual-258.cs.uct.ac.za>
2022-08-16 16:34:56 +0200

    Adds more tables

--- db_setup.sql
+++ db_setup.sql
@@ -4,3 +4,25 @@ INSERT INTO authors (Surname, Initials, Title, Institution, Rating) VALUES ("Ald
 INSERT INTO authors (Surname, Initials, Title, Institution, Rating) VALUES ("Atemkeng Teufack", "M", "Dr", "Rhodes University", "Y" );
 INSERT INTO authors (Surname, Initials, Title, Institution, Rating) VALUES ("Bagula", "BA", "Prof", "University of the Western Cape", "C" );
 INSERT INTO authors (Surname, Initials, Title, Institution, Rating) VALUES ("Daramola", "JO", "Prof", "Cape Peninsula University of Technology", "C" );
+
+CREATE TABLE IF NOT EXISTS subFields (ID int AUTO_INCREMENT, subField varChar(255), PRIMARY KEY(ID));
+INSERT INTO subFields (subField) VALUES ("Applied Artificial Intelligence");
+INSERT INTO subFields (subField) VALUES ("Machine Learning");
+INSERT INTO subFields (subField) VALUES ("Speech Processing");
+INSERT INTO subFields (subField) VALUES ("Artificial neural networks");
+INSERT INTO subFields (subField) VALUES ("Machine Vision");
+INSERT INTO subFields (subField) VALUES ("Deep learning")
+INSERT INTO subFields (subField) VALUES ("Artificial Intelligence")
+
+CREATE TABLE IF NOT EXISTS authorSubfieldsMap (authorID varChar(255), subFieldID int);
+INSERT INTO authorSubfieldsMap (authorID, subFieldID) VALUES ("Daramola", 1)
+INSERT INTO authorSubfieldsMap (authorID, subFieldID) VALUES ("Bagula", 7)
+INSERT INTO authorSubfieldsMap (authorID, subFieldID) VALUES ("Atemkeng Teufack", 7 )
+INSERT INTO authorSubfieldsMap (authorID, subFieldID) VALUES ("Atemkeng Teufack", 2 )
+INSERT INTO authorSubfieldsMap (authorID, subFieldID) VALUES ("Atemkeng Teufack", 6 )
+INSERT INTO authorSubfieldsMap (authorID, subFieldID) VALUES ("Aldrich", 2)
+INSERT INTO authorSubfieldsMap (authorID, subFieldID) VALUES ("Aldrich", 4)
+INSERT INTO authorSubfieldsMap (authorID, subFieldID) VALUES ("Aldrich", 5)
+INSERT INTO authorSubfieldsMap (authorID, subFieldID) VALUES ("Aharonson", 2)
+INSERT INTO authorSubfieldsMap (authorID, subFieldID) VALUES ("Aharonson", 3)
+INSERT INTO authorSubfieldsMap (authorID, subFieldID) VALUES ("Aharonson", 1)
