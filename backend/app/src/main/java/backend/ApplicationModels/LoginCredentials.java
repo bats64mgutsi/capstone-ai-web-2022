6eeb772536e4a46313666e6050c89e68eb4b94d7
julio <julioderose11@gmail.com>
2022-08-24 15:22:12 +0200

    added application models

---
+++ backend/app/src/main/java/backend/ApplicationModels/LoginCredentials.java
@@ -0,0 +1,11 @@
+package backend.ApplicationModels;
+
+public class LoginCredentials {
+    public String username;
+    public String password;
+
+    public LoginCredentials(String username, String password){
+        this.username=username;
+        this.password=password;
+    }
+}
