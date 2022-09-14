c783d6c29411bed2654cf14af67073c6f8030960
Batandwa Mgutsi <b.mgutsi@gmail.com>
2022-09-14 13:48:06 +0200

    Implements HttpClient

--- backend/app/src/main/java/backend/httpClient/HttpClient.java
+++ backend/app/src/main/java/backend/httpClient/HttpClient.java
@@ -1,7 +1,15 @@
 package backend.httpClient;
 
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+
 public class HttpClient {
     public String fetchWebPage(String path) {
-        return "";
+        try {
+            return Jsoup.connect("").get().html();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
