2e9f6becbc4d7a7217381d04066fee27aabe82b9
Batandwa Mgutsi <b.mgutsi@gmail.com>
2022-09-16 14:49:36 +0200

    Reverts frontend client

--- client/src/api/urls.ts
+++ client/src/api/urls.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const getWebsocketUrl = (apiUrl: string) => {
     const urlObj = new URL(apiUrl);
     const wsProtocol = urlObj.protocol.replace("http", "ws");
@@ -6,13 +5,4 @@ const getWebsocketUrl = (apiUrl: string) => {
 };
   
 export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8001";
-=======
-const getWebsocketUrl = (apiUrl: string) => {
-    const urlObj = new URL(apiUrl);
-    const wsProtocol = urlObj.protocol.replace("http", "ws");
-    return `${wsProtocol}//${urlObj.host}`;
-};
-  
-export const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8001";
->>>>>>> master
 export const WEBSOCKET_URL = getWebsocketUrl(API_URL);
--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { ref, computed } from "vue";
 import { Author, AuthorProfile } from "../core/types";
 
@@ -43,49 +42,3 @@ const store = {
 };
   
 export default store;
-=======
-import { ref, computed } from "vue";
-import { Author, AuthorProfile } from "../core/types";
-
-/**
- * Bundle and expose variables/functionalities 
- * to do with the managing of authors in the system
- */
- function initAuthors() {
-    const authors = ref<Author[]>([]);
-    const currentlySelectedAuthorId = ref("");
-    const currentlySelectedAuthorProfile = ref<AuthorProfile>();
-  
-    function setAuthors(list: Author[]) {
-        authors.value = list;
-    }
-
-    function setCurrentlySelectedAuthor(id: string) {
-        currentlySelectedAuthorId.value = id;
-    }
-  
-    const currentlySelectedAuthor = computed(() =>
-      authors.value.find((p) => `${p.surname}` === currentlySelectedAuthorId.value)
-    );
-
-    function setCurrentlySelectedAuthorProfile(profile: AuthorProfile) {
-        currentlySelectedAuthorProfile.value = profile;
-    }
-  
-    return {
-        setAuthors,
-        setCurrentlySelectedAuthor,
-        setCurrentlySelectedAuthorProfile,
-        currentlySelectedAuthor,
-        currentlySelectedAuthorId,
-        currentlySelectedAuthorProfile,
-        authors,
-    };
-}
-
-const store = {
-    ...initAuthors(),
-};
-  
-export default store;
->>>>>>> master
