b321f69a18512f2d10ab72c2f1fe48c2e53b18bb
julio <julioderose11@gmail.com>
2022-09-19 12:47:21 +0200

    keyword table

---
+++ backend/app/src/main/java/backend/Tables/AiKeyword.java
@@ -0,0 +1,23 @@
+package backend.Tables;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AiKeyword extends SqlTable {
+    public void insertKeyword(String keyword) throws SQLException{
+
+    }
+
+    public List<String> listAll() throws SQLException{
+        List<String> keywords = new ArrayList<>();
+
+        return keywords;
+    }
+
+    public void clearAll() throws SQLException{
+
+    }
+    
+
+}
