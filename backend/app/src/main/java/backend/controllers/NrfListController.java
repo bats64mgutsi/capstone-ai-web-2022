4d6e3abdaf1ba3946b3e68fc3a8e8022dc69de0d
Batandwa Mgutsi <b.mgutsi@gmail.com>
2022-09-20 13:21:40 +0200

    Removes unused import

--- backend/app/src/main/java/backend/controllers/NrfListController.java
+++ backend/app/src/main/java/backend/controllers/NrfListController.java
@@ -18,7 +18,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Stream;
 
 public class NrfListController {
     final Logger logger = Logger.getLogger(NrfListController.class.getName());
