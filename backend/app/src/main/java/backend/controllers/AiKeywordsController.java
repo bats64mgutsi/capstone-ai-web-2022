503e1e044da85908af6d65c25f6d9ceca43afb4c
Batandwa Mgutsi <b.mgutsi@gmail.com>
2022-09-21 14:06:27 +0200

    Ai keywords backend fixes

--- backend/app/src/main/java/backend/controllers/AiKeywordsController.java
+++ backend/app/src/main/java/backend/controllers/AiKeywordsController.java
@@ -1,15 +1,22 @@
 package backend.controllers;
 
-import backend.DatabaseModels.*;
 import backend.Locator;
 import backend.Tables.*;
+
 import java.sql.SQLException;
 import java.util.*;
 
 public class AiKeywordsController {
     final AiKeywordsTable aiKeywordsTable = (AiKeywordsTable) Locator.instance.get(AiKeywordsTable.class);
 
-public List<String> listAikeywords() throws SQLException {
-    return aiKeywordsTable.listAll();
-}
+    public List<String> listAiKeywords() throws SQLException {
+        return aiKeywordsTable.listAll();
+    }
+
+    public void setAiKeywords(List<String> aiKeywords) throws SQLException {
+        aiKeywordsTable.clearAll();
+        for(final String keyword : aiKeywords) {
+            aiKeywordsTable.insertKeyword(keyword);
+        }
+    }
 }
