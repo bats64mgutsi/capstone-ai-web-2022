af168c4ab8a1faf13da0cd25806fc3e066e845ad
Batandwa Mgutsi <b.mgutsi@gmail.com>
2022-09-23 10:21:13 +0200

    Adds a launch script and rmeoves unused files

---
+++ launch.sh
@@ -0,0 +1,9 @@
+
+# This is the launch script for the AI-Web application
+# Use this script to start the application.
+
+# This script assumes that the frontend has already been built
+# and ready to be served by the java server.
+
+cd backend
+./gradlew run
