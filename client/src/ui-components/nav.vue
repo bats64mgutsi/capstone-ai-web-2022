955486283364b8621cca52cb984acfb5b4f459b5
Vainqueur Mahirwe <bjthe2nd@gmail.com>
2022-09-24 22:21:31 +0200

    Redirect to logo page on logo click

--- client/src/ui-components/nav.vue
+++ client/src/ui-components/nav.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
         <div class="container flex flex-wrap justify-between items-center mx-auto">
-            <a href="https://flowbite.com/" class="flex items-center">
+            <a href="/" class="flex items-center">
                 <img src="../assets/logo.png" class="mr-3 h-6 sm:h-9" alt="AI Web Logo">
                 <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">AI Web</span>
             </a>
