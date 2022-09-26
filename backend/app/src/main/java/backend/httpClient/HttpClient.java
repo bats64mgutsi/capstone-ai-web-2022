de7d3d13b253eff55c555f07c2888e4029d861b0
Batandwa Mgutsi <b.mgutsi@gmail.com>
2022-09-26 14:20:23 +0200

    Allow web scraping with multiple webscraping keys

--- backend/app/src/main/java/backend/httpClient/HttpClient.java
+++ backend/app/src/main/java/backend/httpClient/HttpClient.java
@@ -1,23 +1,33 @@
 package backend.httpClient;
 
+import org.apache.commons.io.IOUtils;
 import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.SocketTimeoutException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HttpClient {
+    private Map<String, Boolean> webScrapingKeyToExpiredStatus = new HashMap<>();
+
     public String lastOut = "";
     public String fetchWebPage(String path) {
+        ensureWebScrapingKeysLoaded();
+        final String proxyUrl = makeProxyUrl(path);
+
         try {
-            final Document doc = Jsoup.connect(makeProxyUrl(path)).get();
+            final Document doc = Jsoup.connect(proxyUrl).get();
             String pageHtml = doc.html();
-            if(pageHtml.contains("Please show you're not a robot")){
+            if(pageHtml.contains("Please show you're not a robot")) {
                 // Re-fetch web page with a different proxy server.
-                pageHtml = fetchWebPage(path);
+                return fetchWebPage(path);
             }
 
             return lastOut = pageHtml;
@@ -29,6 +39,13 @@ public class HttpClient {
                 } catch (InterruptedException ex) {
                     throw new RuntimeException(ex);
                 }
+            } else if(e.getStatusCode() == 401) {
+                for(final Map.Entry<String, Boolean> entry: webScrapingKeyToExpiredStatus.entrySet()) {
+                    if(proxyUrl.contains(entry.getKey())) {
+                        entry.setValue(true);
+                    }
+                }
+                return fetchWebPage(path);
             }
         } catch(SocketTimeoutException e) {
             // Retry
@@ -42,6 +59,43 @@ public class HttpClient {
 
     private String makeProxyUrl(String path) {
         final String urlEscapedPath = URLEncoder.encode(path, StandardCharsets.UTF_8);
-        return "https://api.webscrapingapi.com/v1?api_key=uMcuS7qXrNYf0pRVp0WhSP6tdrYw4mB0&url="+urlEscapedPath;
+        final String key = getNextKey();
+        return "https://api.webscrapingapi.com/v1?api_key="+key+"&url="+urlEscapedPath;
+    }
+
+    private String getNextKey() {
+        for(final Map.Entry<String, Boolean> key: webScrapingKeyToExpiredStatus.entrySet()) {
+            if(!key.getValue()) {
+                // Key is not expired.
+                return key.getKey();
+            }
+        }
+
+        throw new RuntimeException("All keys are expired.");
+    }
+
+    private void ensureWebScrapingKeysLoaded() {
+        if(webScrapingKeyToExpiredStatus.isEmpty()) {
+            String resourcesDirString = "src/main/data";
+
+            try {
+                if(!System.getProperty("user.dir").endsWith("/app")) {
+                    // When run from the command line, the working directory is backend/app
+                    // But when run from IntelliJ IDE, the working directory is backend/
+                    resourcesDirString = "app/"+ resourcesDirString;
+                }
+
+                final Path resourcesDir = Path.of("", resourcesDirString);
+                Path file = resourcesDir.resolve("web_scraping_keys.txt");
+                final String webScrapingKeysAsString = IOUtils.toString(new FileReader(file.toFile()));
+                final String[] webScrapingKeys = webScrapingKeysAsString.split("\n");
+
+                for(final String key: webScrapingKeys) {
+                    webScrapingKeyToExpiredStatus.put(key, false);
+                }
+            } catch(IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
